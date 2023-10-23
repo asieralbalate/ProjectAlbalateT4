28fa50aa7dee51baa243f4739962c3b03c7755f4
asieralbalate <alualb1634@ieselcaminas.org>
2023-10-24 00:13:10 +0200

    Fourth Commit

--- app/src/main/java/com/example/proyectoalbalate/ui/theme/Theme.kt
+++ app/src/main/java/com/example/proyectoalbalate/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.proyectoalbalatet4.ui.theme
+package com.example.proyectoalbalate.ui.theme
 
 import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -8,7 +8,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -38,7 +37,6 @@ private val LightColorScheme = lightColorScheme(
 fun ProyectoAlbalateT4Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
